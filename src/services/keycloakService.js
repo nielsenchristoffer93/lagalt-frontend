d618d7f6896fb59367b11005bb8367fc220ef6ce
pontusrohdenexperis <88532449+pontusrohdenexperis@users.noreply.github.com>
2021-09-16 13:35:14 +0200

    Update keycloakService.js

--- src/services/keycloakService.js
+++ src/services/keycloakService.js
@@ -62,7 +62,7 @@ const postNewUser = async() => {
         return await fetch(`${BASE_API_URL}users`, {
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin' : '*'
+                'Access-Control-Allow-Origin' : 'https://lagalt-frontend-gbg.herokuapp.com/*'
             },
             method: "POST",
             body:JSON.stringify({
