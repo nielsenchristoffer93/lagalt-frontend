a34c88976aad58af9c0852012a24b5f2d00b3ff6
pontusrohdenexperis <88532449+pontusrohdenexperis@users.noreply.github.com>
2021-09-22 11:14:06 +0200

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,14 @@
 /**
   * Deployed
   */
-//export const BASE_API_URL = "https://lagalt-experis-backend.herokuapp.com/api/v1/";
-//export const BASE_URL = "https://lagalt-experis-backend.herokuapp.com"
+export const BASE_API_URL = "https://lagalt-experis-backend.herokuapp.com/api/v1/";
+export const BASE_URL = "https://lagalt-experis-backend.herokuapp.com"
 
 
 
 /**
   * Local
   */
-export const BASE_API_URL = "http://localhost:8080/api/v1/";
-export const BASE_URL = "http://localhost:8080"
+//export const BASE_API_URL = "http://localhost:8080/api/v1/";
+//export const BASE_URL = "http://localhost:8080"
 
